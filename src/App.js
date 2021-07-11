6577d3c7bdc6bc14d1632803b89e4baed1857043
sereendaqamsih <sereen.aldaqamsih@gmail.com>
2021-07-11 17:11:06 +0300

    save lab 11

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './Footer';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
+import Profile from './components/Profile';
+import MyFavoriteBooks from './BestBooks';
+import { withAuth0 } from '@auth0/auth0-react';
+import Login from './Login';
+
 
 class App extends React.Component {
 
@@ -15,20 +19,25 @@ class App extends React.Component {
     return(
       <>
         <Router>
-          <IsLoadingAndError>
+          {/* <IsLoadingAndError> */}
             <Header />
             <Switch>
               <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {this.props.auth0.isAuthenticated? <MyFavoriteBooks/>:<Login/>}
+
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route exact path="/Profile">
+          <Profile/>
+        </Route>
             </Switch>
             <Footer />
-          </IsLoadingAndError>
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     );
   }
 }
 
-export default App;
+export default withAuth0 (App);
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
+import { withAuth0 } from '@auth0/auth0-react';
+
 import { Link } from "react-router-dom";
 import './Header.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 class Header extends React.Component {
   render() {
@@ -10,11 +14,12 @@ class Header extends React.Component {
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
+        <Link to="/Profile">Profile</Link>
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+     {this.props.auth0.isAuthenticated? <LogoutButton/>:<LoginButton/>}
       </Navbar>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
